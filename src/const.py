ca5a68c27c3358d64b45d4f095ee58fd1aa4ff30
Roman Kushnirenko <roman.kushnirenko@tomtom.com>
2023-02-02 11:40:30 +0100

    Add some refactoring

--- src/const.py
+++ src/const.py
@@ -1,4 +1,17 @@
+from typing import Dict
+
+
 class Const:
     measurements_per_day: int = 8
     batch_size: int = 1
     epochs: int = 50
+    meteorological_stations: Dict[str, int] = {
+        'Teteriv': 33228,
+        'Chornobyl': 33231,
+        'Fastiv': 33339,
+        'Kyiv': 33345,
+        'Boryspil': 33347,
+        'Yagotyn': 33356,
+        'Bila Tserkva': 33464,
+        'Myronivka': 33466
+    }
--- src/main.py
+++ src/main.py
@@ -1,30 +1,18 @@
-from typing import Dict
 import numpy as np
 
 from prepare_data import DataLoader
 from model import Model
 from const import Const
 
-meteorological_stations: Dict[str, int] = {
-    'Teteriv': 33228,
-    'Chornobyl': 33231,
-    'Fastiv': 33339,
-    'Kyiv': 33345,
-    'Boryspil': 33347,
-    'Yagotyn': 33356,
-    'Bila Tserkva': 33464,
-    'Myronivka': 33466
-}
 
+def rmse(y_true: np.array, y_pred: np.array) -> np.float32:
+    return np.sqrt(np.mean(np.square(y_pred - y_true)))
 
-def mse(y_true: np.array, y_pred: np.array) -> np.float32:
-    return np.mean(np.square(y_pred - y_true))
-
-
-shape = (-1, Const.measurements_per_day, 1)
 
 if __name__ == '__main__':
-    loader: DataLoader = DataLoader(meteorological_stations['Kyiv'])
+    shape = (-1, Const.measurements_per_day, 1)
+
+    loader: DataLoader = DataLoader(Const.meteorological_stations['Kyiv'])
 
     train_observations: np.array
     train_forecasts: np.array
@@ -42,15 +30,16 @@ if __name__ == '__main__':
     model.train(train_forecasts, train_observations,
                 (val_forecasts, val_observations))
 
-    test_loader: DataLoader = DataLoader(meteorological_stations['Teteriv'])
+    test_loader: DataLoader = DataLoader(
+        Const.meteorological_stations['Teteriv'])
     test_observations: np.array
     test_forecasts: np.array
-    test_observations, test_forecasts = loader.get_data()
-    print("RMSE without ML:", mse(test_forecasts, test_observations))
+    test_observations, test_forecasts = test_loader.get_data()
+    print("RMSE without ML:", rmse(test_forecasts, test_observations))
 
     test_forecasts = test_forecasts.reshape(shape)
 
-    print("RMSE with ML (prediction):", mse(
+    print("RMSE with ML (prediction):", rmse(
         model.forecast(test_forecasts), test_observations))
-    print("RMSE with ML (evaluation):", model.test(
-        test_forecasts, test_observations))
+    print("RMSE with ML (evaluation):", np.sqrt(model.test(
+        test_forecasts, test_observations)))
