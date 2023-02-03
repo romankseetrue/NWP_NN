69ed3c0b42c02bbac0b42d0a0ceeda2679ab03a2
Roman Kushnirenko <roman.kushnirenko@tomtom.com>
2023-02-03 13:50:52 +0100

    Refactor main.py

--- src/main.py
+++ src/main.py
@@ -1,46 +1,34 @@
 import numpy as np
 
-from prepare_data import Query, TrainValDataLoader, TestDataLoader
+from prepare_data import Query, TestDataLoader
 from model import Model
-from const import Const
 
 
 def rmse(y_true: np.array, y_pred: np.array) -> np.float32:
     return np.sqrt(np.mean(np.square(y_pred - y_true)))
 
 
-if __name__ == '__main__':
-    shape = (-1, Const.measurements_per_day, 1)
+def get_forecast_nn(test_query: Query, model: Model, file_to_save: str) -> None:
+    test_loader: TestDataLoader = TestDataLoader(test_query)
 
-    train_observations: np.array
-    train_forecasts: np.array
-    train_observations, train_forecasts = TrainValDataLoader(
-        [Query('Kyiv', '2012-07-01', '2013-07-01')]).get_data()
-    train_forecasts = train_forecasts.reshape(shape)
-
-    val_observations: np.array
-    val_forecasts: np.array
-    val_observations, val_forecasts = TrainValDataLoader(
-        [Query('Kyiv', '2013-07-01', '2013-11-02')]).get_data()
-    val_forecasts = val_forecasts.reshape(shape)
-
-    model: Model = Model()
-    model.train(train_forecasts, train_observations,
-                (val_forecasts, val_observations))
-
-    test_loader: TestDataLoader = TestDataLoader(Query('Teteriv'))
     test_observations: np.array
     test_forecasts: np.array
     test_observations, test_forecasts = test_loader.get_data()
     print("RMSE without ML:", rmse(test_forecasts, test_observations))
 
-    test_forecasts = test_forecasts.reshape(shape)
-
     test_forecasts_nn: np.array = model.forecast(test_forecasts)
     test_loader.update(test_forecasts_nn)
-    test_loader.save_to_file("results.csv")
+    test_loader.save_to_file(file_to_save)
 
     print("RMSE with ML (prediction):", rmse(
         test_forecasts_nn, test_observations))
     print("RMSE with ML (evaluation):", np.sqrt(model.test(
         test_forecasts, test_observations)))
+
+
+if __name__ == '__main__':
+    model: Model = Model()
+    model.train([Query('Kyiv', '2012-07-01', '2013-07-01')],
+                [Query('Kyiv', '2013-07-01', '2013-11-02')])
+
+    get_forecast_nn(Query('Teteriv'), model, "results.csv")
--- src/model.py
+++ src/model.py
@@ -5,8 +5,9 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, GRU, Input
 
-from typing import Tuple
+from typing import List
 from const import Const
+from prepare_data import Query, TrainValDataLoader
 
 
 def create_model() -> keras.Model:
@@ -28,22 +29,37 @@ class Model:
 
         self.__model: keras.Model = create_model()
         self.__model.compile(loss='mse', optimizer='adam')
+        self.__input_data_shape = (-1, Const.measurements_per_day, 1)
 
-    def train(self, inp: np.array, out: np.array, val_data: Tuple[np.array, np.array]) -> tf.keras.callbacks.History:
+    def train(self, train_data_queries: List[Query], val_data_queries: List[Query]) -> tf.keras.callbacks.History:
+        train_observations: np.array
+        train_forecasts: np.array
+        train_observations, train_forecasts = TrainValDataLoader(
+            train_data_queries).get_data()
+        train_forecasts = train_forecasts.reshape(self.__input_data_shape)
+
+        val_observations: np.array
+        val_forecasts: np.array
+        val_observations, val_forecasts = TrainValDataLoader(
+            val_data_queries).get_data()
+        val_forecasts = val_forecasts.reshape(self.__input_data_shape)
+
         return self.__model.fit(
-            x=inp,
-            y=out,
+            x=train_forecasts,
+            y=train_observations,
             batch_size=Const.batch_size,
             epochs=Const.epochs,
             verbose=Const.verbose,
-            validation_data=val_data
+            validation_data=(val_forecasts, val_observations)
         )
 
     def test(self, inp: np.array, out: np.array) -> np.float32:
+        inp = inp.reshape(self.__input_data_shape)
         return self.__model.evaluate(x=inp,
                                      y=out,
                                      verbose=Const.verbose)
 
     def forecast(self, inp: np.array) -> np.array:
+        inp = inp.reshape(self.__input_data_shape)
         return self.__model.predict(x=inp,
                                     verbose=Const.verbose)
